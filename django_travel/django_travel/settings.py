aa9a7c987eb66a8509f9cfa5056a9b7a851f0df3
Ryan Hutchinson <RyanKenjiHutch@gmail.com>
2024-07-14 10:57:47 -0700

    Updated settings.py to include staticfiles

--- django_travel/django_travel/settings.py
+++ django_travel/django_travel/settings.py
@@ -117,11 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+import os
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Path to collect static files
 
-import os
+# import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
