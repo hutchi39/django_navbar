47171912d15ec008a4b4fda4d4c2ecf8a87d769f
Ryan Hutchinson <RyanKenjiHutch@gmail.com>
2024-07-14 11:14:55 -0700

    Updated settings.py to include staticfiles

--- django_travel/django_travel/settings.py
+++ django_travel/django_travel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-ottln%2npaf8#824uaht3o%4x-$o&+_4*h_f8mnq1cu&*f647l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
